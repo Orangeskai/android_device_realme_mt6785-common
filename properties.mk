009c7bf18fe4cf2ab5e8c2bf8ae131017abb3dc3
Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
2021-06-19 09:34:18 +0530

    mt6785-common: Enable GBoard rounded corners Signed-off-by: ManshuTyagi <himanshut567@gmail.com> Signed-off-by: officialputuid <officialputuid@hack.id>

--- properties.mk
+++ properties.mk
@@ -32,7 +32,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # GBoard
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.com.google.ime.kb_pad_port_b=1
+    ro.com.google.ime.kb_pad_port_b=1 \
+    ro.com.google.ime.corner_key_r=35
 
 # IMS
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
